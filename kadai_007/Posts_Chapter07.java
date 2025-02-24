231497a43daf704fe8146d64ceae5667bdc48cba
中島ももな <mona@s-chan2.local>
2025-02-24 15:16:43 +0900

    kadai_007の更新3

--- kadai_007/Posts_Chapter07.java
+++ kadai_007/Posts_Chapter07.java
@@ -2,7 +2,6 @@ package kadai_007;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -12,56 +11,43 @@ public class Posts_Chapter07 {
 	public static void main(String[] args) {
 		
 		Connection con = null;
-		PreparedStatement statement = null;
-		Statement statement1 = null;
-		//Statement statement = null;
-		
-		//投稿データ
-		String[][] posts = {
-				{"1003","2023-02-08","昨日の夜は徹夜でした・・・","13"},
-				{"1002","2023-02-08","お疲れ様です！","12"},
-				{"1003","2023-02-09","今日も頑張ります！","18"},
-				{"1001","2023-02-09","無理は禁物ですよ！","17"},
-				{"1002","2023-02-10","明日から連休ですね！","20"}
-		};
+		Statement statement = null;
 		
 		try {
 			con = DriverManager.getConnection(
 					"jdbc:mysql://localhost/challenge_java",
 					"root",
-					""
+					"nyanko48"
 					);
 			
-			System.out.println("データベース接続成功");
+			System.out.println("データベース接続成功" + con);
 			
 			//SQLクエリを準備
-			String sql = "INSERT INTO posts (user_id,posted_at,post_content,likes) VALUES (?,?,?,?);";
-			statement = con.prepareStatement(sql);
-			//String sql = "SELECT * FROM posts WHERE user_id = 1002;";
-			
-			int rowCnt = 0;
-			for(int i = 0; i < posts.length; i++) {
-				statement.setString(1, posts[i][0]);
-				statement.setString(2, posts[i][1]);
-				statement.setString(3, posts[i][2]);
-				statement.setString(4, posts[i][3]);	
-				
-				rowCnt = statement.executeUpdate();
-				
-			}	
-				System.out.println("レコード追加を実行します");
-				System.out.println(posts.length + "件のレコードが追加されました");
+			statement = con.createStatement();
+			String sqlInsert = 
+					"""
+					INSERT INTO posts (user_id, posted_at, post_content, likes) VALUES
+					(1003, '2023-02-08', '昨日の夜は徹夜でした・・・', 13),
+					(1002, '2023-02-08', 'お疲れ様です！', 12),
+					(1003, '2023-02-09', '今日も頑張ります！', 18),
+					(1001, '2023-02-09', '無理は禁物ですよ！', 17),
+					(1002, '2023-02-10', '明日から連休ですね！', 20);
+					""";
+
+				//SQLクエリを実行
+			System.out.println("レコード追加を実行します");
+			int rowCnt = statement.executeUpdate(sqlInsert);
+			System.out.println(rowCnt/* posts.length */ + "件のレコードが追加されました");
 	
-			String sql1 = "SELECT posted_at,post_content,likes FROM posts WHERE user_id = 1002;";
-			statement1 = con.createStatement();
+			//データ検索のSQLクエリを実行
+			String sqlSelect = "SELECT posted_at,post_content,likes FROM posts WHERE user_id = 1002;";
+			ResultSet result = statement.executeQuery(sqlSelect);
 			
-			//SQLクエリを実行
-			ResultSet result = statement1.executeQuery(sql1);
+			//SQLクエリの実行結果を抽出
 			System.out.println("ユーザーIDが1002のレコードを検索しました");
 			
 			//SQLクエリの実行結果を抽出
 			while(result.next()) {
-				//int userId = result.getInt("user_id");
 				Date postedAt = result.getDate("posted_at");
 				String content = result.getString("post_content");
 				int likes = result.getInt("likes");
@@ -74,9 +60,6 @@ public class Posts_Chapter07 {
 		} finally {
 			if(statement != null) {
 				try {statement.close();} catch(SQLException ignore) {}
-			}
-			if(statement1 != null) {
-				try {statement1.close();} catch(SQLException ignore) {}
 			}
 			if(con != null) {
 				try {con.close();} catch(SQLException ignore) {}
